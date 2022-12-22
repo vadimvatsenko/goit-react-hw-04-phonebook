8ed79309e4c64693ae77fe8c0394cfc352a6e09b
vadimvatsenko <69381493+vadimvatsenko@users.noreply.github.com>
2022-12-22 09:53:41 +0200

    22.12.2022 09:53

--- src/components/form/form.js
+++ src/components/form/form.js
@@ -1,12 +1,26 @@
 //DONE
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import style from './form.module.scss'
 import { nanoid } from 'nanoid';
 import PropTypes from "prop-types";
 
+//катомный хук для localStorage
+const useLocalStorage = (key, defaultValue) => {
+    const [state, setState] = useState(() => {
+        return JSON.parse(window.localStorage.getItem(key)) ?? defaultValue;
+    });
+
+    useEffect(() => {
+        window.localStorage.setItem(key, JSON.stringify(state));
+    }, [key, state]);
+    return [state, setState]
+}
+
 const Form = ({ onSubmitHandle }) => {
-    const [name, setName] = useState('');
-    const [number, setNumber] = useState('');
+    const [name, setName] = useLocalStorage('name', '');
+    const [number, setNumber] = useLocalStorage('number', '');
+    // const [name, setName] = useState('');
+    // const [number, ] = useState('');
 
     const nameInputId = nanoid();
     const numberInputId = nanoid();
@@ -84,78 +98,3 @@ Form.propTypes = {
 
 export {Form}
 
-// export default class Form extends Component {
-//     nameInputId = nanoid();
-//     numberInputId = nanoid();
-    
-//     state = {
-//     name: '',
-//     number: ''
-//     }
-
-
-//     handleChange = (e) => {
-//         const { name, value } = e.currentTarget;
-//         this.setState({
-//             [name]: value
-//         });
-//     };
-
-//         resetForm = () => {
-//             this.setState(
-//                 {
-//                     name: '',
-//                     number: ''
-//                 }
-//             );
-//     }
-
-//     handleSubmit = (e) => {
-//         e.preventDefault();
-//         this.props.onSubmitHandle(this.state);
-//         this.resetForm();
-
-//     };
-
-
-
-//     render() {
-//         const {name, number} = this.state
-//         return (
-//             <form className={style.form__section } onSubmit={this.handleSubmit}>
-//                 <label className={style.label__header} htmlFor={this.nameInputId}>Name</label>
-//                 <input
-//                     onChange={this.handleChange}
-//                     className={style.label__input}
-//                     id={this.nameInputId}
-//                     type="text"
-//                     name="name"
-//                     value={name}
-//                     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-//                     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-//                     required
-//                 />
-//                 <label className={style.label__header} htmlFor={this.numberInputId}>Number</label>
-//                 <input
-//                     onChange={this.handleChange}
-//                     className={style.label__input}
-//                     id={this.numberInputId}
-//                     type="tel"
-//                     name="number"
-//                     value={number}
-//                     pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-//                     title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-//                     required
-//                 />
-
-//                 <button className={style.phonebook__button} type="submit">Add contact</button>
-//             </form>
-        
-//         );  
-//     };
-// }
-
-// Form.propTypes = {
-//   onSubmitHandle: PropTypes.func.isRequired,
-  
-// };
